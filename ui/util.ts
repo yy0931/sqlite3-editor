ef221f997d46fba28483f6af5d88fec1c249d52c
yy0931 <54441600+yy0931@users.noreply.github.com>
2023-01-15 20:45:05 +0900

    Reformat code

--- ui/util.ts
+++ ui/util.ts
@@ -1,6 +1,10 @@
 import create, { StoreApi, UseBoundStore } from "zustand"
 
-export const createStore = <S, A>(initialState: S, actions: (
-    set: UseBoundStore<StoreApi<S>>["setState"],
-    get: UseBoundStore<StoreApi<S>>["getState"],
-) => A) => create<S & A>()((set, get) => ({ ...initialState, ...actions(set, get) }))
+export const createStore = <S, A>(
+    initialState: S,
+    actions: (set: UseBoundStore<StoreApi<S>>["setState"], get: UseBoundStore<StoreApi<S>>["getState"]) => A,
+) =>
+    create<S & A>()((set, get) => ({
+        ...initialState,
+        ...actions(set, get),
+    }))
