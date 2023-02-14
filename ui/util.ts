6af2496e907cdf24a57a66454e665fd99459a642
yy0931 <54441600+yy0931@users.noreply.github.com>
2023-02-14 22:54:31 +0900

    Rename a function

--- ui/util.ts
+++ ui/util.ts
@@ -3,12 +3,16 @@ import create, { StoreApi, UseBoundStore } from "zustand"
 export const createStore = <S, A>(name: string, initialState: S, actions: (
     set: UseBoundStore<StoreApi<S>>["setState"],
     get: UseBoundStore<StoreApi<S>>["getState"],
-) => A) => fixZustandHMR(name, create<S & A>()((set, get) => ({ ...initialState, ...actions(set, get) })))
+) => A) =>
+    hmr(name, create<S & A>()((set, get) => ({
+        ...initialState,
+        ...actions(set, get)
+    })))
 
-const fixZustandHMR = <T>(key: string, store: T): T => {
+const hmr = <T>(key: string, store: T): T => {
     // @ts-ignore
     if (import.meta.env.DEV) {
-        return (window as any)[`zustand-${key}`] ?? ((window as any)[`zustand-${key}`] = store)
+        return (window as any)[key] ?? ((window as any)[key] = store)
     }
     return store
 }
