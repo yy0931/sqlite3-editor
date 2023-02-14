2ba673095724c67709de1e81541105bf304961f8
yy0931 <54441600+yy0931@users.noreply.github.com>
2023-02-14 22:51:08 +0900

    Generate vendor.LICENSE.txt

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -1,12 +1,27 @@
 import { defineConfig } from "vite"
 import preact from "@preact/preset-vite"
+import license from "rollup-plugin-license"
+import path from "path"
 
 export default defineConfig({
-    plugins: [preact()],
+    plugins: [
+        preact(),
+
+        // https://github.com/vitejs/vite/discussions/7722#discussioncomment-4007436
+        license({
+            thirdParty: {
+                output: path.resolve(__dirname, "./dist/assets/vendor.LICENSE.txt"),
+            },
+        }),
+    ],
     build: {
         target: "esnext",
     },
     esbuild: {
-        logOverride: { 'this-is-undefined-in-esm': 'silent' }
+        logOverride: { 'this-is-undefined-in-esm': 'silent' },
+
+        // https://github.com/vitejs/vite/discussions/7722#discussioncomment-4007436
+        banner: '/*! licenses: /assets/vendor.LICENSE.txt */',
+        legalComments: 'none',
     },
 })
