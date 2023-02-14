724a7e13fc3b1b564cb0eb2deb0483519495f633
yy0931 <54441600+yy0931@users.noreply.github.com>
2023-02-14 22:50:07 +0900

    Fix an issue where undo and redo on inputs were not functioning

--- ui/keybindings.ts
+++ ui/keybindings.ts
@@ -40,6 +40,19 @@ const moveSelectionColumn = (delta: number) => {
 // https://gist.github.com/yy0931/14f854aae53dfd4c090fab389b448844
 type Code = "Abort" | "Again" | "AltLeft" | "AltRight" | "ArrowDown" | "ArrowLeft" | "ArrowRight" | "ArrowUp" | "AudioVolumeDown" | "AudioVolumeMute" | "AudioVolumeUp" | "Backquote" | "Backslash" | "Backspace" | "BracketLeft" | "BracketRight" | "BrightnessUp" | "BrowserBack" | "BrowserFavorites" | "BrowserForward" | "BrowserHome" | "BrowserRefresh" | "BrowserSearch" | "BrowserStop" | "CapsLock" | "Comma" | "ContextMenu" | "ControlLeft" | "ControlRight" | "Convert" | "Copy" | "Cut" | "Delete" | "Digit0" | "Digit1" | "Digit2" | "Digit3" | "Digit4" | "Digit5" | "Digit6" | "Digit7" | "Digit8" | "Digit9" | "Eject" | "End" | "Enter" | "Equal" | "Escape" | "F1" | "F10" | "F11" | "F12" | "F13" | "F14" | "F15" | "F16" | "F17" | "F18" | "F19" | "F2" | "F20" | "F21" | "F22" | "F23" | "F24" | "F3" | "F4" | "F5" | "F6" | "F7" | "F8" | "F9" | "Find" | "Fn" | "Help" | "Home" | "Insert" | "IntlBackslash" | "IntlRo" | "IntlYen" | "KanaMode" | "KeyA" | "KeyB" | "KeyC" | "KeyD" | "KeyE" | "KeyF" | "KeyG" | "KeyH" | "KeyI" | "KeyJ" | "KeyK" | "KeyL" | "KeyM" | "KeyN" | "KeyO" | "KeyP" | "KeyQ" | "KeyR" | "KeyS" | "KeyT" | "KeyU" | "KeyV" | "KeyW" | "KeyX" | "KeyY" | "KeyZ" | "Lang1" | "Lang2" | "Lang3" | "Lang4" | "Lang5" | "LaunchApp1" | "LaunchApp2" | "LaunchMail" | "MediaPlayPause" | "MediaSelect" | "MediaStop" | "MediaTrackNext" | "MediaTrackPrevious" | "MetaLeft" | "MetaRight" | "Minus" | "NonConvert" | "NumLock" | "Numpad0" | "Numpad1" | "Numpad2" | "Numpad3" | "Numpad4" | "Numpad5" | "Numpad6" | "Numpad7" | "Numpad8" | "Numpad9" | "NumpadAdd" | "NumpadComma" | "NumpadDecimal" | "NumpadDivide" | "NumpadEnter" | "NumpadEqual" | "NumpadMultiply" | "NumpadParenLeft" | "NumpadParenRight" | "NumpadSubtract" | "OSLeft" | "OSRight" | "Open" | "PageDown" | "PageUp" | "Paste" | "Pause" | "Period" | "Power" | "PrintScreen" | "Props" | "Quote" | "ScrollLock" | "Select" | "Semicolon" | "ShiftLeft" | "ShiftRight" | "Slash" | "Sleep" | "Space" | "Tab" | "Undo" | "Unidentified" | "VolumeDown" | "VolumeMute" | "VolumeUp" | "WakeUp"
 
+const createKeyboardShortcutMatcher = (ev: KeyboardEvent) => (pattern: `${"Ctrl + " | "" | "(Ctrl +) "}${"Shift + " | "" | "(Shift +) "}${"Alt + " | "" | "(Alt +) "}${Code}`): boolean => {
+    const m = /^(Ctrl \+ |\(Ctrl \+\) |)(Shift \+ |\(Shift \+\) |)(Alt \+ |\(Alt \+\) |)(.*?)$/.exec(pattern)
+    if (m === null) { throw new Error() }
+    const [_, c, s, a, code] = m
+    if (c === "Ctrl + " && !ev.ctrlKey) { return false }
+    if (c === "" && ev.ctrlKey) { return false }
+    if (s === "Shift + " && !ev.shiftKey) { return false }
+    if (s === "" && ev.shiftKey) { return false }
+    if (a === "Alt + " && !ev.altKey) { return false }
+    if (a === "" && ev.altKey) { return false }
+    return ev.code === code
+}
+
 export const onKeydown = async (ev: KeyboardEvent) => {
     if (useTableStore.getState().isConfirmDialogVisible) {
         return
@@ -47,22 +60,11 @@ export const onKeydown = async (ev: KeyboardEvent) => {
     try {
         const state = editor.useEditorStore.getState()
         const singleClick = isInSingleClickState()
-        const key = (pattern: `${"Ctrl + " | "" | "(Ctrl +) "}${"Shift + " | "" | "(Shift +) "}${"Alt + " | "" | "(Alt +) "}${Code}`): boolean => {
-            const m = /^(Ctrl \+ |\(Ctrl \+\) |)(Shift \+ |\(Shift \+\) |)(Alt \+ |\(Alt \+\) |)(.*?)$/.exec(pattern)
-            if (m === null) { throw new Error() }
-            const [_, c, s, a, code] = m
-            if (c === "Ctrl + " && !ev.ctrlKey) { return false }
-            if (c === "" && ev.ctrlKey) { return false }
-            if (s === "Shift + " && !ev.shiftKey) { return false }
-            if (s === "" && ev.shiftKey) { return false }
-            if (a === "Alt + " && !ev.altKey) { return false }
-            if (a === "" && ev.altKey) { return false }
-            return ev.code === code
-        }
         const inputFocus = ev.target instanceof HTMLElement && !ev.target.matches("table textarea") && ev.target.matches("label, button, input, textarea, select, option")
         const findWidgetFocus = ev.target instanceof HTMLInputElement && ev.target.id === "findWidget"
 
         const p = () => ev.preventDefault()
+        const key = createKeyboardShortcutMatcher(ev)
         if (findWidgetFocus && key("Escape")) {
             p()
             ev.target.blur()
@@ -161,4 +163,17 @@ export const onKeydown = async (ev: KeyboardEvent) => {
     } catch (err) {
         console.error(err)
     }
+}
+
+// Fixes a problem where the user is unable to undo/redo on inputs due to VSCode calling preventDefault() on the events.
+if (window.acquireVsCodeApi) {
+    window.addEventListener("keydown", (ev) => {
+        if (!(document.activeElement instanceof HTMLInputElement || document.activeElement instanceof HTMLTextAreaElement)) { return }
+        const key = createKeyboardShortcutMatcher(ev)
+        if (key("Ctrl + KeyZ")) {
+            document.execCommand('undo', false)
+        } else if (key("Ctrl + Shift + KeyZ") || key("Ctrl + KeyY")) {
+            document.execCommand('redo', false)
+        }
+    })
 }
