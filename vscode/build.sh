9a03900cdb9389524babdc1ccb9992eae5e4cde6
yy0931 <54441600+yy0931@users.noreply.github.com>
2023-02-16 23:09:28 +0900

    Add comments

--- vscode/build.sh
+++ vscode/build.sh
@@ -2,11 +2,13 @@
 
 script_dir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 
+# Build ../ui/ and copy the bundle ./webview
 cd "$script_dir" || exit 1
 rm -rf ./webview
 cd ../ui || exit 1
 npm run build
-
 cd "$script_dir" || exit 1
 cp -r ../ui/dist ./webview
+
+# Build ./extension.ts
 npx esbuild extension.ts --bundle --outfile=extension.js --target=esnext --format=cjs --platform=node --minify --external:vscode
